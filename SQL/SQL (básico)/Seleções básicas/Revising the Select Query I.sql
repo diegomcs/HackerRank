e3de0021d48c7d81b3a8d422c3a042289520aa29
diegomcs <diegocs.art@gmail.com>
2022-03-03 11:04:30 -0300

    Update

--- SQL/SQL (básico)/Seleções básicas/Revising the Select Query I.sql
+++ SQL/SQL (básico)/Seleções básicas/Revising the Select Query I.sql
@@ -1,17 +1,14 @@
---   ________________________
---         Informações
+--  Informações
 
 --  Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
 --  Dificuldade: Easy
 --  Pontuação: 10
 --  DBMS: mySQL
 
---  Consulte todas as colunas de todas as cidades americanas na tabela CITY com populações maiores que 100000.O CountryCode para a América é USA.
+  Consulte todas as colunas de todas as cidades americanas na tabela CITY com populações maiores que 100000.O CountryCode para a América é USA.
 
-SELECT
-    *
-FROM
-    CITY
+SELECT *
+FROM CITY
 WHERE
     POPULATION > 100000 AND
-    COUNTRYCODE = "USA"
+    COUNTRYCODE = 'USA'
