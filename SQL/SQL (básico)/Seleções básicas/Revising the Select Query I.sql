423d5882f57dc7f749ef1064a8d4af4111e683a6
diegomcs <diegocs.art@gmail.com>
2022-03-03 11:11:05 -0300

    Update

--- SQL/SQL (básico)/Seleções básicas/Revising the Select Query I.sql
+++ SQL/SQL (básico)/Seleções básicas/Revising the Select Query I.sql
@@ -1,22 +1,18 @@
 
- Informações:
+Informações:
 
---  Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
---  Dificuldade: Easy
---  Pontuação: 10
---  DBMS: mySQL
-_____________
+    Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem 
+    Dificuldade: Easy
+    Pontuação: 10
+    DBMS: mySQL
 
 Problema:
 
---  Consulte todas as colunas de todas as cidades americanas na tabela CITY com populações maiores que 100000.O CountryCode para a América é USA.
-_____________
+    Consulte todas as colunas de todas as cidades americanas na tabela CITY com populações maiores que 100000.O CountryCode para a América é USA.
 
 Solução:
 
-SELECT *
-FROM CITY
-WHERE
-    POPULATION > 100000 AND
-    COUNTRYCODE = 'USA'
+    SELECT *
+    FROM CITY
+    WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA'
 
