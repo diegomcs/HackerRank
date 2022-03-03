bb5f9c7ce9e5ead59416d44e127d68c708169f1c
diegomcs <diegocs.art@gmail.com>
2022-03-03 16:44:42 -0300

    Update

--- SQL/SQL (básico)/Seleções básicas/09 - Weather Observation Station 4.sql
+++ SQL/SQL (básico)/Seleções básicas/09 - Weather Observation Station 4.sql
@@ -1,23 +1,19 @@
 
 Informações:
 
---  Link do problema: https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+--  Link do problema: https://www.hackerrank.com/challenges/weather-observation-station-4/problem
 --  Dificuldade: Easy
---  Pontuação: 15
---  DBMS: Oracle / MySQL
+--  Pontuação: 10
+--  DBMS: Oracle
 
 Problema:
 
---  Consulte uma lista de nomes de CITY de STATION para cidades que tenham um ID par. Imprima os resultados em qualquer ordem, mas exclua duplicatas da resposta.
+--  Encontre a diferença entre o número total de entradas e o número total de entradas distintas na tabela CITY.
+--  Por exemplo, se houver três registros na tabela sendo eles 'New York', 'New York' e 'Bengalaru' então existem 2 nomes de cidades diferentes: 'New York' e 'Bengalaru'. 
+--  A consulta deve retornar 1, sendo o numero total de entradas - total de entradas distintas.
 
 Solução:
 
-SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
-FROM STATION;
+    SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+    FROM STATION
 
--- ========================
---       Explanation
--- ========================
-
--- DISTINCT() used to avoid duplication
--- COUNT() used to return the number of rows in a table
---
+++ SQL/SQL (básico)/Seleções básicas/10 - Weather Observation Station 5.sql
@@ -0,0 +1,42 @@
+
+Informações:
+
+--  Link do problema: https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+--  Dificuldade: Easy
+--  Pontuação: 10
+--  DBMS: Oracle
+
+Problema:
+
+--  Consulte as duas cidades em STATION com os nomes mais curtos e mais longos na tabela CITY, bem como seus respectivos comprimentos (ou seja: número de caracteres no nome). Se houver mais de uma cidade menor ou maior, escolha a que vem primeiro quando ordenada alfabeticamente.
+
+Entrada de amostra
+
+--  Por exemplo, CITY tem quatro entradas: DEF, ABC, PQRS e WXY .
+
+Saída de Amostra
+
+--  ABC 3
+--  PQRS 4
+
+Explicação
+
+--  Quando ordenados alfabeticamente, os nomes em CITY são listados como ABC, DEF, PQRS e WXY. O nome mais longo é PQRS, mas existem opções para a cidade nomeada mais curta. Escolha ABC , porque vem primeiro em ordem alfabética.
+
+Observação
+
+--  Você pode escrever duas consultas separadas para obter a saída desejada. Não precisa ser uma única consulta.
+
+Solução:
+
+    SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY), CITY ASC
+    LIMIT 1;
+
+    SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY) DESC
+    LIMIT 1;
+
+
